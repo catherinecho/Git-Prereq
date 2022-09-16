5e5cdb239e11c18dd55a573f0d19d8fe37e19e3e
catherine cho <ccho2@hwemail.com>
2022-09-15 22:43:53 -0700

    Set up tester
    tried testing the init function

--- src/git/CatherineTester.java
+++ src/git/CatherineTester.java
@@ -2,6 +2,13 @@ package git;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -10,15 +17,36 @@ class CatherineTester {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+        Path p = Paths.get("test.txt");
+        try {
+            Files.writeString(p, "example", StandardCharsets.ISO_8859_1);
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		
+		File f = new File("test.txt");
+		f.delete();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testInit() throws IOException {
+		index ind = new index(); 
+		ind.init();
+		
+		File f = new File("index.txt");
+		assertTrue(f.exists());
+		
+		Path path = Paths.get("objects");
+		assertTrue(Files.exists(path));
+		
+		
 	}
+	
 
 }
